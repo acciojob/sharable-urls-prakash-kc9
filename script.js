44057c1183e0d68ca181f39f88101a80669d5b33
Prakash Choudhary <09prakash20@gmail.com>
2025-10-20 17:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+let h3 = querySelector("#url");
+
+let submit = querySelector("#submit");
+
+
+submit.addEventListener('click' , ()=>{
+	let name = document.querySelector('#name').value;
+	let year = document.querySelector('#year').value;
+	let temp = `https://localhost:8080/${name} ${year}` 
+	h3.textContent = 
+})
